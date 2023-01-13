da48300ba763effc6ed098ae47474bc621c2e689
Scoozy <s.safanovich@gmail.com>
2023-01-12 21:08:57 -0800

    aaaaaaa

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,22 @@
 const DB = require("sigidb");
 const db = DB("db.sqlite");
 global.db = db;
-
+let members = []
 class Person{
-  constructor(name,branch,leader){
-    this.name = name
+  constructor(fullName,branch,leader){
+    this.fullName = fullName
     this.branch = branch;
     this.leader = leader;
   }
+  get fullName() {
+    return 
+  }
 }
 let newPerson = new Person("Paul K.", "Build", false)
+function createPerson(fullMame, branch, leader) {
+  let me = Object.create(Person)
+  me.name = "Sergei"
+}
 console.log(newPerson)
 // db.set("person", newPerson)
 // db.set("test", "hey");
--- src/Components/Maincontent/Main.jsx
+++ src/Components/Maincontent/Main.jsx
@@ -12,12 +12,20 @@ class Main extends React.Component {
         startDate:"int",
         finishDate:"int",
         robotFinished:"28 days"
-     }
-    }
+      }
+  }
+  get getInput() {
+    let firstName = document.querySelector(".firstName").value
+    console.log(firstName)
+  }
+
     render() {
       return(
         <div>
-                  <h2>This is when robot is supposed to be done {this.state.robotFinished} </h2>
+          <h2>This is when robot is supposed to be done {this.state.robotFinished} </h2>
+          <label htmlFor="">First name</label>
+          <input type="text" className="firstName" />
+          <button onClick={getInput()}></button>
         </div>
       ) 
     }
