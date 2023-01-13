72d3e71ddcb3519a4f9d00a3288420c138910b73
Scoozy <s.safanovich@gmail.com>
2023-01-12 19:36:14 -0800

    yay a got it

--- Server/server.js
+++ Server/server.js
@@ -2,23 +2,27 @@ const DB = require("sigidb");
 const db = DB("db.sqlite");
 global.db = db;
 
-
 class Person{
   constructor(name,branch,leader){
-    this.name = ""
-    this.branch = "";
-    this.leader = false;
+    this.name = name
+    this.branch = branch;
+    this.leader = leader;
   }
 }
-let newPerson = new Person("Paul K.","Build",false)
-db.set("Person", newPerson)
+let newPerson = new Person("Paul K.", "Build", false)
+console.log(newPerson)
+// db.set("person", newPerson)
+// db.set("test", "hey");
+console.log('====================================');
+console.log(db.get("person"));
+console.log('====================================');
 
 
-let task = {
-  branch:"",
-  members:[person],
-}
+// let task = {
+//   branch: "",
+//   description:"",
+//   members:[""],
+// }
 
-db.set("tasks",task)
+// db.set("tasks",task)
 
-db.set()
